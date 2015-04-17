9be3b14498c56bc066dbae30c6f5207c9314d30c
David M <dave@radiusnetworks.com>
2015-04-17 11:10:05 -0400

    wip-basically working

---
+++ lib/serial_port_reader.rb
@@ -0,0 +1,15 @@
+class SerialPortReader
+  def initialize(port)
+    @port = port
+  end
+
+  def on_data
+    EM.run do
+      serial = EventMachine.open_serial(port, 115200, 8, nil, 1)
+      serial.on_data do |data|
+        yield data
+      end
+    end
+  end
+
+end
